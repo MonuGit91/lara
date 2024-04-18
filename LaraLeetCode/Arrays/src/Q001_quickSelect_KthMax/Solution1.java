ab174efb9dbc10012190c41f6fa804696b37191c
Monu Choudhary <74929383+MonuGit91@users.noreply.github.com>
2024-04-18 11:30:48 +0530

    Update Solution1.java

--- LaraLeetCode/Arrays/src/Q001_quickSelect_KthMax/Solution1.java
+++ LaraLeetCode/Arrays/src/Q001_quickSelect_KthMax/Solution1.java
@@ -16,13 +16,13 @@ class Solution1 {
         }
         swap(arr, pointer, right);
         
-		int rank = pointer-left+1;
-		if(rank == k) return arr[pointer];
-		else if(rank > k) {
+	int rank = pointer-left+1;
+	if(rank == k) return arr[pointer];
+	else if(rank > k) {
             return quickSellectAlgo(arr, left, pointer-1, k);   
         }
 	else {
-			return quickSellectAlgo(arr, pointer+1, right, k-rank);
+		return quickSellectAlgo(arr, pointer+1, right, k-rank);
 	}
 
     }
